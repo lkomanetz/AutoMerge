05bf5c8d602523b9794db8801a078db681ec8eee
Logan Komanetz <Logan Komanetz>
2017-02-20 13:11:18 -0600

    Initial commit.

---
+++ src/Library.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace ClassLibrary {
+
+    public static class AutoMerge {
+
+        public static void Merge<T>(
+            ref T destination,
+            T source,
+            PropertyInfo propInfo = null
+        ) where T : class, new() {    
+
+        }
+
+    }
+
+}
