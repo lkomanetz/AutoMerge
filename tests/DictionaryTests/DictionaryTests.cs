773d74dd4e5e249ff2836e4490ccf53d35cd8bb4
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-02-24 10:57:00 -0600

    getting multiple test projects working.

---
+++ tests/DictionaryTests/DictionaryTests.cs
@@ -0,0 +1,16 @@
+using AutoMerger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoMerger.UnitTests {
+
+	[TestClass]
+	public class DictionaryTests {
+
+		[TestMethod]
+		public void DictionaryWithValuesSucceeds() {
+			Assert.IsTrue(true);
+		}
+
+	}
+
+}
