934b5c1fafd8da6836ddbfa6ccf5e95e441d3b89
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-04-18 09:10:25 -0500

    Adding tests for dictionaries.

--- tests/AutoMergeTests/AutoMergeTests.cs
+++ tests/AutoMergeTests/AutoMergeTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
-namespace ConsoleApplication {
+namespace AutoMerger.UnitTests {
 
     public class AutoMergeTests {
 
--- tests/DictionaryTests/DictionaryTests.cs
+++ tests/DictionaryTests/DictionaryTests.cs
@@ -1,4 +1,5 @@
 using AutoMerger;
+using System.Collections.Generic;
 using Xunit;
 
 namespace AutoMerger.UnitTests {
@@ -9,6 +10,41 @@ namespace AutoMerger.UnitTests {
 		public void DictionaryWithValuesSucceeds() {
 			Assert.True(true);
 		}
+
+		[Theory]
+		[InlineDataAttribute(0,0)]
+		[InlineDataAttribute(1,0)]
+		[InlineDataAttribute(2,1)]
+		[InlineDataAttribute(3,500)]
+		public void DictionaryWithPrimitiveSucceeds(int key, int value) {
+			DictionaryClass testA = new DictionaryClass();
+			DictionaryClass testB = new DictionaryClass();
+			int iterations = 10;
+
+			for (int i = 0; i < iterations; ++i) {
+				testA.PrimitiveDictionary.Add(key++, value++);
+				testB.PrimitiveDictionary.Add(key++, value++);
+			}
+
+			AutoMerge.Merge(ref testA, testB);
+
+			foreach (var kvp in testA.PrimitiveDictionary) {
+				int valueB = testB.PrimitiveDictionary[kvp.Key];
+				Assert.True(kvp.Value == valueB);
+			}
+		}
+
+	}
+
+	internal class DictionaryClass {
+
+		public DictionaryClass() {
+			this.PrimitiveDictionary = new Dictionary<int, int>();
+			this.StringDictionary = new Dictionary<int, string>();
+		}
+
+		public IDictionary<int, int> PrimitiveDictionary { get; set; }
+		public IDictionary<int, string> StringDictionary { get; set; }
 
 	}
 
